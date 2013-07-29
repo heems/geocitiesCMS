aac6619314628e1971291cd1ce971b5c8f82df81
Karthik Bala <drmelonhead@gmail.com>
2013-07-29 13:48:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocities::Application.config.secret_token = 'b8e1799b1c1decc287e31ec3cafb7087ab36d85af1520ff198c913be313bd569e49035509c9081c4448da3394b00f70373e806872d3747f334901f1480331766'
